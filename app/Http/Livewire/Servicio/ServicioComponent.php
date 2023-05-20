a6c557b1cbaa48e19cf7dcb906db33af28b7af9f
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2023-05-20 07:10:02 -0400

    serv

---
+++ app/Http/Livewire/Servicio/ServicioComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Servicio;
+
+use Livewire\Component;
+
+class ServicioComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.servicio.servicio-component');
+    }
+}
--- resources/views/livewire/servicio/servicio-component.blade.php
+++ resources/views/livewire/servicio/servicio-component.blade.php
@@ -4,7 +4,7 @@
             <div class="card my-4">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                        <h6 class="text-white text-capitalize ps-3">Tipos de servicios</h6>
+                        <h6 class="text-white text-capitalize ps-3">Servicios</h6>
                     </div>
                     {{-- boton añadir --}}
                     <div class=" me-3 my-3 text-end">
